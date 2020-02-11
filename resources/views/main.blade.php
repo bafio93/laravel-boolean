98578484925bd709e62b150f4badbf0570c6c35a
Previtali_Fabio <fabprev93@gmail.com>
2020-02-11 17:53:14 +0100

    Collegato correttamente un fogli di stile + compilatore che funziona!

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -3,10 +3,11 @@
     <head>
         <meta charset="utf-8">
         <title>Working page</title>
-        <link rel="stylesheet" href="app.css">
+        <link rel="stylesheet" href="{{ asset('app.css') }}">
     </head>
     <body>
         <h1 class="bafbaf">TUTTO PERFETTO!</h1>
         <a href="/faq">VADO ALLE FAQ</a>
+        <script src="{{ asset('app.js') }}" type="text/javascript"></script>
     </body>
 </html>

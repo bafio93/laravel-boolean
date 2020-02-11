f0a0fa3e63a857a00eb2ad5b55f40b98b52b0065
Previtali_Fabio <fabprev93@gmail.com>
2020-02-11 17:28:50 +0100

    Init! (+spippolata con view)

---
+++ resources/views/faq.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+    <head>
+        <meta charset="utf-8">
+        <title>Working page</title>
+        <link rel="stylesheet" href="app.css">
+    </head>
+    <body>
+        <h1>PAGINA FAQ</h1>
+        <a href="/">TORNA ALLA PAGINA DI INIZIO</a>
+    </body>
+</html>
---
+++ resources/views/main.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+    <head>
+        <meta charset="utf-8">
+        <title>Working page</title>
+        <link rel="stylesheet" href="app.css">
+    </head>
+    <body>
+        <h1 class="bafbaf">TUTTO PERFETTO!</h1>
+        <a href="/faq">VADO ALLE FAQ</a>
+    </body>
+</html>

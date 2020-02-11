01d0861f1cf19e7aff7853b0958c7d1683b8f308
Previtali_Fabio <fabprev93@gmail.com>
2020-02-11 19:51:32 +0100

    Utilizzo anche SASS, fatto nav!

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -1,3 +1,14 @@
-<nav>
-    <h5>Questo è il menù sopra</h5>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <a href="{{ route('home_page') }}" class="navbar-brand">
+        <img src="https://www.boolean.careers/images/common/logo.png" alt="">
+    </a>
+    <div class="float-right">
+        <ul class="navbar-nav">
+            <li class="nav-item"><a class="nav-link" href="{{ route('home_page') }}">HOME</a></li>
+            <li class="nav-item"><a class="nav-link" href="#">CORSO</a></li>
+            <li class="nav-item"><a class="nav-link" href="#">DOPO IL CORSO</a></li>
+            <li class="nav-item"><a class="nav-link" href="#">LEZIONE GRATUITA</a></li>
+            <li class="baf-cta nav-item"><a class="nav-link" href="#">CANDIDATI ORA</a></li>
+        </ul>
+    </div>
 </nav>
--- resources/views/privacy.blade.php
+++ resources/views/privacy.blade.php
@@ -1,5 +1,7 @@
 @extends("layouts.main")
 @section("content")
+    @include("layouts.nav")
     <h1>PAGINA PRIVACY</h1>
     <a href="{{ route('home_page') }}">TORNA ALLA PAGINA DI INIZIO</a>
+    @include("layouts.footer")
 @endsection

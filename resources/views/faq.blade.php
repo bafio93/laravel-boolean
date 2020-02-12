9581524d17eff75c46bcf214fc473cff82b45356
Previtali_Fabio <fabprev93@gmail.com>
2020-02-12 16:40:10 +0100

    Affinato stile di base della view delle faq.

--- resources/views/faq.blade.php
+++ resources/views/faq.blade.php
@@ -1,7 +1,23 @@
 @extends("layouts.main")
 @section("content")
     @include("layouts.nav")
-    <h1>PAGINA FAQ</h1>
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+    <section class="baf-faq-jumbo">
+        <div class="container-fluid">
+            <h1>Domande frequenti</h1>
+            <p>Le nostre risposte a tutte le tue domande. Non trovi la risposta che stai cercando? <a href="{{ route('nolink_page') }}">Contattaci</a> e saremo felici di darti una mano.</p>
+        </div>
+    </section>
+    <section class="baf-faq-box">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col baf-faq-box-left">
+                        <p>asd</p>
+                </div>
+                <div class="col baf-faq-box-right">
+                        <p>asd</p>
+                </div>
+            </div>
+        </div>
+    </section>
     @include("layouts.footer")
 @endsection
